4b97fad5c480907c0bc1e31ce2fded75231f3a82
gustavocastro7 <gustavocastro73@gmail.com>
2024-05-22 09:45:31 -0300

    Add log solution

--- get_access_token.py
+++ get_access_token.py
@@ -1,42 +1,80 @@
-import json
-import os
-import requests
-import datetime
-from util.json_tools import print_json
+import logging
 
-PATH = "/open_api/v1.3/oauth2/access_token/"
+class pLogger_cl(object):
+    """
+    Logger class for pLogger_cl
+    """
 
-# Function to load authentication details from JSON file
-def load_auth_from_json(json_file_path):
-    with open(json_file_path, 'r') as f:
-        auth_data = json.load(f)
-    return auth_data.get('secret'), auth_data.get('app_id'), auth_data.get('auth_code')
+    def __init__(self, log_file_path):
+        """
+        Constructor for pLogger_cl.
 
-def build_url(path, query=""):
-    # type: (str, str) -> str
-    """
-    Build request URL
-    :param path: Request path
-    :param query: Querystring
-    :return: Request URL
-    """
-    scheme, netloc = "https", "business-api.tiktok.com"
-    return "{0}://{1}{2}?{3}".format(scheme, netloc, path, query)
+        :param log_file_path: Path to log file
+        """
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.DEBUG)
+        self.formatter = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        self.handler = logging.FileHandler(log_file_path, mode='a', encoding='utf-8')
+        self.handler.setFormatter(self.formatter)
+        self.logger.addHandler(self.handler)
 
-def post(json_str):
-    # type: (str) -> requests.Response
-    """
-    Send POST request
-    :param json_str: Args in JSON format
-    :return: Response object
-    """
-    url = build_url(PATH)
-    args = json.loads(json_str)
-    headers = {
-        "Content-Type": "application/json",
-    }
-    rsp = requests.post(url, headers=headers, json=args)
-    return rsp
+    def log(self, msg, level=logging.DEBUG):
+        """
+        Log a message
+
+        :param msg: Message to log
+        :param level: Logging level
+        """
+        self.logger.log(level, msg)
+
+    def debug(self, msg):
+        """
+        Log a debug message
+
+        :param msg: Message to log
+        """
+        self.log(msg, logging.DEBUG)
+
+    def info(self, msg):
+        """
+        Log an info message
+
+        :param msg: Message to log
+        """
+        self.log(msg, logging.INFO)
+
+    def warning(self, msg):
+        """
+        Log a warning message
+
+        :param msg: Message to log
+        """
+        self.log(msg, logging.WARNING)
+
+    def error(self, msg):
+        """
+        Log an error message
+
+        :param msg: Message to log
+        """
+        self.log(msg, logging.ERROR)
+
+    def critical(self, msg):
+        """
+        Log a critical message
+
+        :param msg: Message to log
+        """
+        self.log(msg, logging.CRITICAL)
+
+if __name__ == '__main__':
+    logger = pLogger_cl("log/log.txt")
+    logger.debug("This is a debug message")
+    logger.info("This is an info message")
+    logger.warning("This is a warning message")
+    logger.error("This is an error message")
+    logger.critical("This is a critical message")
 
 def request_access_token():
     # Load authentication details from JSON file
@@ -52,19 +90,16 @@ def request_access_token():
     # Send POST request
     response = post(json.dumps(my_args))
 
-    # Check if request was successful
+    # Log the request result
+    logger = pLogger_cl("log/log.txt")
     if response.status_code == 200 and response.json().get('code') == 0:
-        # Store the access token in a JSON file
-        with open("config/access_token.json", 'w') as f:
-            json.dump(response.json(), f)
+        logger.info("Request access token successful")
     else:
-        # Log the error message to a log file
-        timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        error_msg = f"Error: {response.status_code} - {response.text}"
-        with open("log/log.txt", 'a', encoding='utf-8') as log_file:
-            log_file.write(f"[{timestamp}] {error_msg}\n")
+        logger.error(f"Request access token failed. Status code: {response.status_code}, Text: {response.text}")
 
     print_json(response.json())
 
 if __name__ == '__main__':
     request_access_token()
+
+
